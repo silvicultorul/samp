788cc7233257e48fb865f8cc083244f7e878b087
SyLvy <158816251+sy7vy@users.noreply.github.com>
2024-12-26 22:30:07 +0200

    Update lssiauth.lua

--- lssiauth.lua
+++ lssiauth.lua
@@ -1,1 +1,1 @@
-["SyLwy","ianis","Krogen","DemonMD","Jackye","Valerio","ilviu","Todi","AkenX","Mury","Kareem","PcS","Alexandru.777","Maia_LUP",".Vixen","NeymarJr.","Narcis12","Saracie"]
+["xx","ianis","Krogen","DemonMD","Jackye","Valerio","ilviu","Todi","AkenX","Mury","Kareem","PcS","Alexandru.777","Maia_LUP",".Vixen","NeymarJr.","Narcis12","Saracie"]
